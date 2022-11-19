5464ab4df4de9eb06ff08729d810e96d94360b49
Romain Antunes <romainantunes2003@hotmail.com>
2022-11-19 17:12:44 +0100

    Update des packages.

--- kairos-web/src/app/app.module.ts
+++ kairos-web/src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgChartsModule } from 'ng2-charts';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {NgChartsModule} from 'ng2-charts';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { TimerComponent } from './components/timer/timer.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CalendarComponent} from './components/calendar/calendar.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {TimerComponent} from './components/timer/timer.component';
+import {StatisticsComponent} from './components/statistics/statistics.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {ShareButtonsModule} from '@ngx-share/buttons';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    NgChartsModule
+    NgChartsModule,
+    ShareButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
