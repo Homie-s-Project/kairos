280201a71edf2485f73ca06b4eb3644902e90c90
Romain Antunes <romainantunes2003@hotmail.com>
2022-11-19 16:02:29 +0100

    Update of the StudiesController.cs

--- kairos-api/Kairos.API/Controllers/StudiesController.cs
+++ kairos-api/Kairos.API/Controllers/StudiesController.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Kairos.API.Context;
 using Kairos.API.Models;
 using Kairos.API.Utils;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -11,12 +15,18 @@ namespace Kairos.API.Controllers;
 public class StudiesController : SecurityController
 {
     private readonly IMemoryCache _memoryCache;
+    private readonly KairosContext _context;
 
-    public StudiesController(IMemoryCache memoryCache)
+    public StudiesController(IMemoryCache memoryCache, KairosContext context)
     {
         _memoryCache = memoryCache;
+        _context = context;
     }
 
+    /// <summary>
+    /// send heatbeat to check if user is still studying
+    /// </summary>
+    /// <returns></returns>
     [HttpGet("heartbeat")]
     public async Task<IActionResult> HeartBeat()
     {
@@ -39,12 +49,133 @@ public class StudiesController : SecurityController
         return Ok("Continue studies");
     }
 
-    private void RefreshMemory(string encryptMicrosoftId)
+    /// <summary>
+    /// return the studies with the studies number
+    /// </summary>
+    /// <param name="studiesId">the studies number</param>
+    /// <returns></returns>
+    [HttpGet("/{studiesId}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Studies))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Studies))]
+    public IActionResult GetStudies(string studiesId)
+    {
+        if (string.IsNullOrEmpty(studiesId))
+        {
+            return BadRequest("Studies id not specified");
+        }
+
+        int studiesIdParsed;
+        bool isParsed = int.TryParse(studiesId, out studiesIdParsed);
+        if (!isParsed)
+        {
+            return BadRequest("Studies id is not valid");
+        }
+
+        // TODO: Check qu'il l'utilisateur est bien dans le groupe de l'étude
+        var studies = _context.Studies.FirstOrDefault(s => s.StudiesId == studiesIdParsed);
+        if (studies == null)
+        {
+            return NotFound("Studies not found");
+        }
+
+        return Ok(studies);
+    }
+
+    /// <summary>
+    /// return the number of hours per days of studies
+    /// </summary>
+    [HttpGet("lastWeek/hoursStudied")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LastWeekHoursData))]
+    public IActionResult LastWeekWork()
+    {
+        // TODO: données fictive
+        Dictionary<string, int> data = new Dictionary<string, int>
+        {
+            {"Lundi", GenerateNumberBetween(1, 6)},
+            {"Mardi", GenerateNumberBetween(1, 6)},
+            {"Mercredi", GenerateNumberBetween(1, 6)},
+            {"Jeudi", GenerateNumberBetween(1, 6)},
+            {"Vendredi", GenerateNumberBetween(1, 6)},
+            {"Samedi", GenerateNumberBetween(1, 6)},
+            {"Dimanche", GenerateNumberBetween(1, 6)}
+        };
+
+        return Ok(new LastWeekHoursData(data));
+    }
+
+    /// <summary>
+    /// return the last week hours of studies per label
+    /// </summary>
+    [HttpGet("lastWeek/hoursPerLabel")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LastWeekWorkPerLabel))]
+    public IActionResult LastWeekHoursPerLabel()
+    {
+        // TODO: données fictive
+        Dictionary<string, int> data = new Dictionary<string, int>
+        {
+            {"Science / Math", GenerateNumberBetween(1, 6)},
+            {"Economie", GenerateNumberBetween(1, 6)},
+            {"Allemand", GenerateNumberBetween(1, 6)},
+            {"Anglais", GenerateNumberBetween(1, 6)},
+            {"Informatique", GenerateNumberBetween(1, 6)}
+        };
+
+        return Ok(new LastWeekWorkPerLabel(data));
+    }
+
+    /// <summary>
+    /// return the rate of studying last week
+    /// </summary>
+    [HttpGet("lastWeek/rate")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+    public IActionResult LastWeekRate()
+    {
+        // TODO: Valeur factive
+        var hours = GenerateNumberBetween(-50, 50);
+
+        return Ok(hours);
+    }
+
+    /// <summary>
+    /// refresh memory cache
+    /// </summary>
+    /// <param name="encryptServiceId">the encrypted service id from the oauth provider (google, microsoft)</param>
+    private void RefreshMemory(string encryptServiceId)
     {
         // Entrée du cache qui est égale à 5min.
         var cacheEntryOption = new MemoryCacheEntryOptions()
             .SetSlidingExpiration(TimeSpan.FromMinutes(5));
 
-        _memoryCache.Set(encryptMicrosoftId, DateTime.Now, cacheEntryOption);
+        _memoryCache.Set(encryptServiceId, DateTime.Now, cacheEntryOption);
+    }
+
+    private int GenerateNumberBetween(int min, int max)
+    {
+        Random random = new Random();
+        return random.Next(min, max);
+    }
+}
+
+public class LastWeekWorkPerLabel
+{
+    public Dictionary<string, int>.KeyCollection Labels { get; set; }
+    public Dictionary<string, int>.ValueCollection Hours { get; set; }
+
+    public LastWeekWorkPerLabel(Dictionary<string, int> data)
+    {
+        Labels = data.Keys;
+        Hours = data.Values;
+    }
+}
+
+public class LastWeekHoursData
+{
+    public Dictionary<string, int>.KeyCollection DayOfWeek { get; set; }
+    public Dictionary<string, int>.ValueCollection Hours { get; set; }
+
+    public LastWeekHoursData(Dictionary<string, int> data)
+    {
+        DayOfWeek = data.Keys;
+        Hours = data.Values;
     }
 }
---
+++ kairos-api/Kairos.API/Models/StudiesDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kairos.API.Models;
+
+public class StudiesDto
+{
+    public StudiesDto(int studiesId, string studiesNumber, string studiesTime, DateTime studiesCreatedDate,
+        List<Label> studiesLabels, Group group)
+    {
+        StudiesId = studiesId;
+        StudiesNumber = studiesNumber;
+        StudiesTime = studiesTime;
+        StudiesCreatedDate = studiesCreatedDate;
+        StudiesLabels = studiesLabels;
+        Group = group;
+    }
+
+    public StudiesDto(Studies studies)
+    {
+        StudiesId = studies.StudiesId;
+        StudiesNumber = studies.StudiesNumber;
+        StudiesTime = studies.StudiesTime;
+        StudiesCreatedDate = studies.StudiesCreatedDate;
+        StudiesLabels = studies.Labels as List<Label>;
+        Group = studies.Group;
+    }
+
+    public int StudiesId { get; set; }
+    public string StudiesNumber { get; set; }
+    public string StudiesTime { get; set; }
+    public DateTime StudiesCreatedDate { get; set; }
+    public List<Label> StudiesLabels { get; set; }
+    public Group Group { get; set; }
+}
