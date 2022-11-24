108f1844cdd8683c52c2396c9b4aaa9883280e29
Romain Antunes <romainantunes2003@hotmail.com>
2022-11-24 08:23:48 +0100

    Update config

--- kairos-web/src/app/app.module.ts
+++ kairos-web/src/app/app.module.ts
@@ -2,16 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {NgChartsModule} from 'ng2-charts';
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { TimerComponent } from './components/timer/timer.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ErrorComponent } from './components/error/error.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CalendarComponent} from './components/calendar/calendar.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {TimerComponent} from './components/timer/timer.component';
+import {StatisticsComponent} from './components/statistics/statistics.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {ErrorComponent} from './components/error/error.component';
+import {ShareButtonModule} from "ngx-sharebuttons/button";
 
 @NgModule({
   declarations: [
@@ -26,9 +25,9 @@ import { ErrorComponent } from './components/error/error.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ShareButtonModule,
     FontAwesomeModule,
     NgChartsModule,
-    ShareButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
