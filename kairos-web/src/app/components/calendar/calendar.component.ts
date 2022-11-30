4e74bedf71c24e06d159ca28b23acf05355c6def
Alexandre Botta <03alexbotta@gmail.com>
2022-11-30 21:22:38 +0100

    Correction redirection du bouton edit
    Pas encore fonctionnel a continuer

--- kairos-web/src/app/components/calendar/calendar.component.ts
+++ kairos-web/src/app/components/calendar/calendar.component.ts
@@ -2,6 +2,7 @@ import { Component, Renderer2 } from '@angular/core';
 import { faPencil, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { CalendarService } from 'src/app/services/calendar/calendar.service';
 import { NavbarService } from 'src/app/services/navbar/navbar.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calendar',
@@ -12,7 +13,16 @@ export class CalendarComponent{
   faPencil = faPencil;
   faXmark = faXmark;
 
-  constructor(public nav: NavbarService, public CalendarService: CalendarService, private renderer: Renderer2) { 
+  constructor(public nav: NavbarService, public CalendarService: CalendarService, private renderer: Renderer2, private _router: Router) { 
     this.nav.showBackButton();
+  }
+
+  goToEvent = (eventId: number|undefined) => {
+    if (typeof eventId == undefined) {
+      alert('L\'event ne peut pas être "undefined"')
+      return 
+    }
+
+    this._router.navigate(['/calendar', eventId])
   }
 }
