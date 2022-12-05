8ed98b07698bc6f4eb259782596061aa0f7e12a4
WilliamDevv <wpasquier61@gmail.com>
2022-12-05 18:55:09 +0100

    Update auth.service.ts

--- kairos-web/src/app/services/auth/auth.service.ts
+++ kairos-web/src/app/services/auth/auth.service.ts
@@ -7,25 +7,46 @@ import { UserModel } from 'src/app/models/user.model';
   providedIn: 'root'
 })
 export class AuthService {
+  user: any;
 
   //constructor() { }
   constructor(private cookie: CookieService, private http: HttpClient) { }
 
   isLoggedIn = (): boolean => {
-    if (this.cookie.get('jwt') != "") {
-      return true;
-    }
+    var auth_token = this.cookie.get('jwt');
+    var isLogged = true;
     
-    return false;
+    // if (auth_token != "") {
+      
+    //   return true;
+    // }
+    const headers = { headers: new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${auth_token}`
+    })};
+
+    this.http
+    .get('http://localhost:5000/User/me', headers)
+    .subscribe(response => {
+      this.user = response;
+      console.log(this.user);
+    }, error => {
+      isLogged = false;
+      console.log(error.status);
+      console.log(error.statusText);
+    });
+    
+    return isLogged;
   }
 
+  // A finaliser et utiliser pour la page de profil
   // getProfileAsync = (): any => {
   //   var currentUser: any;
   //   const auth_token = this.cookie.get('jwt')
   //   const headers = new HttpHeaders ( {
   //     "Content-Type": "application/json",
   //     "Authorization": `Bearer ${auth_token}`
-  //   })
+  //   });-
     
   //   const requestOptions = { headers: headers };
 
