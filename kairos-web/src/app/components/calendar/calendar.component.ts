7a31da1a130672684ca917cb67bcb8eef4eb9ee3
WilliamDevv <wpasquier61@gmail.com>
2022-12-18 11:05:37 +0100

    Remove useless call

--- kairos-web/src/app/components/calendar/calendar.component.ts
+++ kairos-web/src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faPencil, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { CalendarService } from 'src/app/services/calendar/calendar.service';
 import { NavbarService } from 'src/app/services/navbar/navbar.service';
@@ -12,9 +12,7 @@ export class CalendarComponent implements OnInit {
   faPencil = faPencil;
   faXmark = faXmark;
 
-  constructor(public nav: NavbarService, public calendar: CalendarService, private renderer: Renderer2) { 
-    this.renderer.removeClass(document.body, 'landing-background');
-    this.renderer.removeClass(document.getElementById('app-container'), 'centered');
+  constructor(public nav: NavbarService, public calendar: CalendarService) { 
     this.nav.showBackButton();
   }
 
